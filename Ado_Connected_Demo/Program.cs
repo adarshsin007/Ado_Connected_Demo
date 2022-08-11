769f86c39adf4c1dcf04f61a2744ac18bc1fde93
adarshsin007 <singhadarsh53@gmail.com>
2022-08-11 10:19:57 +0530

    Add project files.

---
+++ Ado_Connected_Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Ado_Connected_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-5E1Q7T3\\SQLEXPRESS;initial catalog=PracticeDB;integrated security=true");
+                con.Open();
+                Console.WriteLine("Connection Established");
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
